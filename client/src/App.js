cf07adaa21993794deca0670d96c730494697a0f
Sandeep Chandran <deepsun80@yahoo.com>
2017-10-07 16:57:28 -0500

    Pt-Personal-Project v2

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   }
 
   handleSearchChange = (term) => {    
-    axios.post(`/api/${term}`)
+    axios.post(`api/${term}`)
     .then(res => {
       const urlArray = res.data.hits;
       const imagesArray = [];
@@ -52,7 +52,7 @@ class App extends Component {
   }
 
   handleSignUp = (user) => {
-    axios.post('/users', user)
+    axios.post('users', user)
     .then(res => {
       this.setState({ 
         username: res.data.username,
@@ -65,7 +65,7 @@ class App extends Component {
   }
 
   handleSignIn = (user) => {
-    axios.post('/users/login', user)
+    axios.post('users/login', user)
     .then(res => {
       if (res.data) {
         this.setState({ 
